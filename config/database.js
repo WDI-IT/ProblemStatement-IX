51c194df6a2494cb0c5556e4115e7aa6ee7f3cd5
RasikaGawande <56964294+RasikaGawande@users.noreply.github.com>
2020-06-21 00:33:06 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database:"mongodb+srv://webdevloper:process.env.SECRET_KEY@cluster0-01e1e.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    database:"mongodb+srv://webdevloper:"+process.env.SECRET_KEY+"@cluster0-01e1e.mongodb.net/<dbname>?retryWrites=true&w=majority"
 }
 
